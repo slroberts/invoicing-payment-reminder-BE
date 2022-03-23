8e01451cc0514fef1968409e27dcd50adca855e2
Shomari Roberts <shomariroberts@gmail.com>
2022-03-23 17:55:27 -0400

    .

--- knexfile.js
+++ knexfile.js
@@ -6,15 +6,15 @@ const { DATABASE_URL } = process.env;
 module.exports = {
   development: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/auth.db3',
-    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
     seeds: {
       directory: './database/seeds',
+    },
+    connection: {
+      filename: './database/auth.db3',
     },
     pool: {
       afterCreate: (conn, done) => {
@@ -25,15 +25,20 @@ module.exports = {
 
   testing: {
     client: 'sqlite3',
-    connection: {
-      filename: './database/test.db3',
-    },
     useNullAsDefault: true,
     migrations: {
       directory: './database/migrations',
     },
     seeds: {
       directory: './database/seeds',
+    },
+    connection: {
+      filename: './database/auth.db3',
+    },
+    pool: {
+      afterCreate: (conn, done) => {
+        conn.run('PRAGMA foreign_keys= ON', done);
+      },
     },
   },
 
